8aa27e49e2761d0ff18eca3b6e8cf51e16c923a6
Whiteyeas <gabriel-le-jedi@hotmail.fr>
2017-05-23 17:07:44 +0200

    test

--- Serveur/task.js
+++ Serveur/task.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+
 var pool  = mysql.createPool({
   host     : 'localhost',
   user     : 'root',
@@ -6,78 +7,52 @@ var pool  = mysql.createPool({
   database : 'todolist'
 });
 
-pool.on('release', function (connection) {
-  console.log('Connection %d released', connection.threadId);
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "yroot",
+  password: "",
+  database: "todolist"
 });
 
-.add = function(){
-
-}
-
-.addTask = function(){
-	
-}
-
-.connection = function(email, password){
-	
-}
-
-.verification = function(email){
-
-}
-
-.register = function(email, password){
-
-}
 
 
 
+add = function(){
 
+};
 
+addTask = function(){
+	connection.query("SELECT * FROM users WHERE Name ="+email+" AND Password = "+password, function (error, results, fields){
+	if (err) throw err;		
+};
 
+connection = function(email, password){
+	connection.query("SELECT * FROM users WHERE Name ="+email+" AND Password = "+password, function (error, results, fields){
+		if (err) throw err;
+		if (results != null) {
+			return true;
+		};
+		return false;
+	});
+};
 
-
-
-pool.getConnection(function(err, connection) {
-  	// Use the connection
-  	
-  	for(var i=0; i<100; i++){
-  		console.log('Insert #'+i);
-	  	connection.query('INSERT INTO tasks SET ?', {title: 'test'+i, details:'mes details'+i}, function (error, results, fields) {
-	  		if (error) throw error;
-	  		console.log('Last insertId:'+results.insertId);
-		});
-  	}
-
-	connection.query('UPDATE tasks SET ? WHERE id=1', {title: 'test', details:'mes details'}, function (error, results, fields) {
-  		if (error) throw error;
-  		 console.log(results.changedRows + ' rows updated');
+verification = function(email){
+	connection.query("SELECT * FROM users WHERE Name ="+email, function (error, results, fields){
+		if (err) throw err;
+		if (results != null) {
+			return true;
+		};
+		return false;
 	});
 
-	connection.query('DELETE FROM tasks WHERE status = "done"', function (error, results, fields) {
-  		if (error) throw error;
-  		console.log(results.affectedRows + ' rows deleted');
-	});
+};
+
+register = function(email, password){
+
+};
 
 
-	connection.query('SELECT * FROM tasks', function (error, results, fields) {
-	    // And done with the connection.
-	    connection.release();
-	    for(result in results){
-	    	console.log(results[result].title);
-	    	
-	    }
-	    //console.log(results);
 
-	    //console.log(fields);
 
-	    // Handle error after the release.
-	    if (error) throw error;
 
-	    // Don't use the connection here, it has been returned to the pool.
-	    pool.end(function (err) {
-	  		// all connections in the pool have ended
-		});
-  	});
 
-});
